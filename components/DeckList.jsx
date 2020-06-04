2bb1c717cb924dfd3e3c02db830f8e3d443d9b0d
Tony <tony.krikorian@outlook.com>
2020-06-04 15:50:28 -0400

    Add render deckList

--- components/DeckList.jsx
+++ components/DeckList.jsx
@@ -2,21 +2,44 @@ import React, { useState, useEffect, Fragment } from "react";
 import { Text, View, StyleSheet, FlatList } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { getDecks } from '../utils/API'
-const DeckList = () => {
+import DeckItem from './DeckItem';
 
-    const [deckList, setDeckList] = useState()
+const DeckList = (props) => {
+    const [deckItems, setDeckItems] = useState({})
     useEffect(() => {
-        //Get Decks list from Async Storage
         getDecks().then((result) => {
-            setDeckList(JSON.stringify(result))
+            setDeckItems(JSON.parse(result))
+            console.log(deckItems)
         })
     }, [])
 
-    return (<Fragment>
-        <View>
-            <Text>Hello</Text>
-        </View>
-    </Fragment>);
+
+    const flatList = (decksStorage) => {
+        return (
+            <FlatList
+                data={Object.values(decksStorage)}
+                renderItem={({ item }) => (
+                    <DeckItem {...item} key={item.title} {...props} />
+                )}
+            />
+        );
+    };
+    return <View style={styles.container}>
+        {flatList(deckItems)}
+    </View>;
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginTop: 50,
+    },
+    textNoDeck: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
 export default DeckList;
--- utils/API.js
+++ utils/API.js
@@ -4,20 +4,20 @@ export const DECKS_KEYS = "UdaciFitness:calendar";
 
 
 export function getDecks() {
-    return AsyncStorage.getItem(DECKS_KEYS);
+    return AsyncStorage.getItem(DECKS_KEYS)
 }
 
 export function submitEntry(entry) {
+
     const data = {
         [entry]: {
             title: entry,
             questions: [],
         },
     };
-
-    console.log("Submit entry");
-    console.log(JSON.stringify(data));
     return AsyncStorage.mergeItem(DECKS_KEYS, JSON.stringify(data), (err) => {
         console.log(err);
     }).then(() => { });
 }
+
+
