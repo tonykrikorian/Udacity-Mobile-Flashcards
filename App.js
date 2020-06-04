b3a0eb0f8b0d118c193ba77662c7f23f7eba2794
Tony <tony.krikorian@outlook.com>
2020-06-04 17:59:31 -0400

    Configure redux

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TabNavigator from './components/TabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer, middleware);
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
