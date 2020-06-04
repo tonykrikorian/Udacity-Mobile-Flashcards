0dc224d285f318a5f05c9d6c8a164acee094f356
Tony <tony.krikorian@outlook.com>
2020-06-04 13:29:39 -0400

    Add TabNavigator

---
+++ components/TabNavigator.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { Text, View } from 'react-native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { MaterialIcons } from '@expo/vector-icons'
+
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createMaterialBottomTabNavigator()
+const TabNavigator = () => {
+
+    return (
+        <Tab.Navigator barStyle={{ backgroundColor: "#fff" }}>
+            <Tab.Screen name="Decks" component={HomeScreen} options={{
+                tabBarIcon: () => (<MaterialIcons name="folder" size={24} color="gray" />)
+            }} />
+            <Tab.Screen name="AddDeck" component={SettingsScreen} options={{
+                tabBarIcon: () => (<MaterialIcons name="folder" size={24} color="gray" />)
+            }} />
+        </Tab.Navigator>
+
+    );
+}
+
+export default TabNavigator;
