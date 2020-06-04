0fd6fe82bd47a177895e8c55915a04fb72a34bd4
Tony <tony.krikorian@outlook.com>
2020-06-04 17:53:53 -0400

    Version estable

--- utils/API.js
+++ utils/API.js
@@ -1,23 +1,22 @@
 import { AsyncStorage } from "react-native";
 export const DECKS_KEYS = "UdaciFitness:calendar";
 
-
-
 export function getDecks() {
     return AsyncStorage.getItem(DECKS_KEYS)
 }
 
 export function submitEntry(entry) {
-
+    const xx = getItemId();
+    console.log(xx)
     const data = {
         [entry]: {
             title: entry,
             questions: [],
         },
     };
-    return AsyncStorage.mergeItem(DECKS_KEYS, JSON.stringify(data), (err) => {
-        console.log(err);
-    }).then(() => { });
+    // return AsyncStorage.mergeItem(DECKS_KEYS, JSON.stringify(data), (err) => {
+    //     console.log(err);
+    // }).then(() => { });
 }
 
 
